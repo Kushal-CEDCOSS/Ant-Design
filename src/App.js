6776ff535896a3568f465de058d504d2a0c1d8ee
Kushal <kushaltewari@cedcoss.com>
2022-10-28 11:22:35 +0530

    Ant Design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Table } from 'antd';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Action',
+      dataIndex: '',
+      key: 'x',
+      render: (record) => <Button danger onClick={()=> handleDelete(record)}>Delete</Button>,
+    },
+  ];
+
+  const [data, setData] = useState([
+    {
+      key: 1,
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+    },
+    {
+      key: 2,
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+    },
+    {
+      key: 3,
+      name: 'Not Expandable',
+      age: 29,
+      address: 'Jiangsu No. 1 Lake Park',
+      description: 'This not expandable',
+    },
+    {
+      key: 4,
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+    },
+  ]);
+  
+  const handleDelete = (currentData) => {
+    var temp = [...data];
+    var pos = -1;
+    data.map((item,index) => item.key === currentData.key ? pos = index : null) 
+    temp.splice(pos, 1);
+    setData(temp);
+  }
+
+  return(
+  <Table
+    columns={columns}
+    expandable={{
+      expandedRowRender: (record) => (
+        <p
+          style={{
+            margin: 0,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+          {record.description}
+        </p>
+      ),
+      rowExpandable: (record) => record.name !== 'Not Expandable',
+    }}
+    dataSource={data}
+  />
+  )
+};
 export default App;
